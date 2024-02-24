9d6e0e7c8f86a2a3c28f1c3c075c26a6bc9e05d8
theo.fratia <theofratia22@gmail.com>
2024-02-24 12:49:55 +0100

    style home page

--- projet_dev_mobile/lib/screen/home.dart
+++ projet_dev_mobile/lib/screen/home.dart
@@ -8,10 +8,52 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Projet Flutter Dev Mobile'),
+        body: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Premier champ de texte
+                    SizedBox(
+                      width: 200,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Entrez votre nom',
+                        ),
+                      ),
+                    ),
+                    // Bouton
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Envoyer'),
+                    ),
+                  ],
+                ),
+                // Deuxième champ de texte
+                SizedBox(
+                  width: 200,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Entrez votre email',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        body: Text('Hello World'),
     );
   }
 }
