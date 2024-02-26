2c970d429fc5086c31745aab22e508d8c9ecfd5b
theo.fratia <theofratia22@gmail.com>
2024-02-26 14:15:16 +0100

    home page finish

--- projet_dev_mobile/lib/screen/home.dart
+++ projet_dev_mobile/lib/screen/home.dart
@@ -19,34 +19,104 @@ class Home extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Premier champ de texte
-                    SizedBox(
-                      width: 200,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Entrez votre nom',
+                Transform.scale(
+                    scale: 0.7,
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 100),
+                        child: Image.asset('assets/images/logo.png'))
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Premier champ de texte
+                      SizedBox(
+                        width: 320,
+                        height: 50,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(90),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.7), // Shadow color
+                                spreadRadius: 0,
+                                blurRadius: 7,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              fillColor: Colors.transparent,
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(90),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: 'Quel est votre destination ?',
+                              contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 50.0),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                    // Bouton
-                    SizedBox(
-                      width: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Envoyer'),
-                    ),
-                  ],
+                      // Bouton
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            onPrimary: Colors.black,
+                            elevation: 8,
+                            padding: EdgeInsets.symmetric(horizontal: -20),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20), // Button border radius
+                            ),
+                          ),
+                          child: Center(
+                            child: Icon(Icons.casino_outlined),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                // Deuxième champ de texte
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Entrez votre email',
+                Container(
+                  margin: EdgeInsets.only(bottom: 160),
+                  child: SizedBox(
+                    width: 210,
+                    height: 50,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(90),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.7),
+                            spreadRadius: 0,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.transparent, // Utiliser une couleur transparente pour l'arrière-plan du TextField
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(90),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 50.0),
+                          hintText: 'Un budget ?',
+                        ),
+                      ),
                     ),
                   ),
                 ),
